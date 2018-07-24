d307e6d3a93564f7f697d5edcd223fac9a963ee3
ushamacdonald <usha.macdonald@gmail.com>
2018-07-24 16:04:01 +1200

    Add basic navigation stack

--- app/components/App.js
+++ app/components/App.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
 import store from '../store';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
-
-type Props = {};
-
-export default class App extends Component<Props> {
+class NavigationScreen extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <View style={styles.container}>
-          <Text style={styles.welcome}>Welcome to React Native!</Text>
-          <Text style={styles.instructions}>To get started, edit App.js</Text>
-          <Text style={styles.instructions}>{instructions}</Text>
-        </View>
-      </Provider>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+      </View>
     );
   }
 }
+
+export default createStackNavigator({
+  Navigation: {
+    screen: NavigationScreen
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
